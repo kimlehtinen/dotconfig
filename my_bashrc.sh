2843b2b3faa9d4e9e21eaa020307826f36f9dd6d
Kim Lehtinen <kim@kimlehtinen.com>
2025-05-04 14:55:27 +0300

    Distinct between git branch and kubeps

--- my_bashrc.sh
+++ my_bashrc.sh
@@ -1,10 +1,15 @@
 # DOT CONFIG
-## Git and Kubernetes
-source ~/.config/dotconfig/bash-configs/kube-ps1/kube-ps1.sh
+
+## Git Branch
 parse_git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ [\1]/'
 }
-# [dir] [branch] [kube_context:kube_namespace]
+
+## Kubernetes Context and Namespace
+source ~/.config/dotconfig/bash-configs/kube-ps1/kube-ps1.sh
+
+# Set the prompt to show the current directory, git branch, and Kubernetes context/namespace
+## [dir] [branch] [kube_context:kube_namespace]
 PS1="\[\e[1;92m\][\w]\[\e[33m\]\$(parse_git_branch) \[\033[00m\]\$(kube_ps1)\n$ "
 
 ## Custom Aliases
